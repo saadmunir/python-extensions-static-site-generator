2bcbd0a27c33976b4b39fcb487a2d3a677c9aedf
saadmunir <saad.bin.munir@gmail.com>
2021-06-25 17:07:52 +0100

    Training section 5 v3

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -8,7 +8,7 @@ total_written = 0
 @hooks.register("start_build")
 def start_build():
     global start_time
-    start_time = time()
+    start_time = time.time()
 
 @hooks.register("written")
 def written():
@@ -17,7 +17,7 @@ def written():
 
 @hooks.register("stats")
 def stats():
-    final_time = time - start_time
+    final_time = time.time() - start_time
     average = final_time/ total_written if total_written else 0
     report = "Converted: {}, Time: {:.2f} sec , Avg: {:.4f} sec/file"
     print(report.format(total_written, final_time, average))
