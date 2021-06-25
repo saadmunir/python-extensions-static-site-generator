a7087d72fcd6feaad662167914deab3cbbfe1d2b
saadmunir <saad.bin.munir@gmail.com>
2021-06-25 15:44:04 +0100

    Training section 2 v2

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -2,14 +2,13 @@ import sys, importlib
 from pathlib import Path
 
 def load_module(directory, name):
-    sys.path.insert([0], directory)
+    sys.path.insert(0, directory)
     importlib.import_module(name)
-    sys.path.pop([0])
+    sys.path.pop(0)
 
 def load_directory(directory):
     for path in directory:
-        path.rglob(".py")
-        load_module(directory.as_posix, path.stem)
+        path.rglob(".py").load_module(directory.as_posix, path.stem)
 
 def load_bundled():
     directory = Path(__file__).parent/"extensions"
