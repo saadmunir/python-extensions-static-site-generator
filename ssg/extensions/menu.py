02ee57c72e501fce2a9c1964698b0077e6e75df4
saadmunir <saad.bin.munir@gmail.com>
2021-06-25 16:44:52 +0100

    Training section 4 v3

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -4,15 +4,15 @@ files = []
 
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
-    valid = lambda p: p.isinstance(parsers.ResourceParser)
+    valid = lambda p: not isinstance(p, parsers.ResourceParser)
     for path in source.rglob("*"):
-        for parser in path.suffix:
-            if parser.valid_file_ext:
+        for parser in list(filter(valid, site_parsers)):
+            if parser.valid_file_ext(path.suffix):
                 files.append(path)
 
 @hooks.register("generate_menu")
 def generate_menu(html, ext):
     template = '<li><a href="{}{}">{}</a></li>'
-    menu_item = lambda name, ext: template.format(name,ext, name.title())
-    menu = "\n".join([menu_item for path in files if menu_item(path.stem, ext) in menu_item])
+    menu_item = lambda name, ext: template.format(name, ext, name.title())
+    menu = "\n".join([menu_item(path.stem, ext) for path in files])
     return "<ul>\n{}<ul>\n{}".format(menu, html)
